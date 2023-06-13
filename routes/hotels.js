1e8b1adfee37620de08abe6165f91f68d28b27db
Amine <88909931+lilamme21@users.noreply.github.com>
2023-06-13 17:00:28 +0000

    Add files via upload

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,1 +1,22 @@
+import express from "express";
+import { createHotel, deleteHotel, getAllHotels, getSingleHotel, updateHotel } from "../controllers/hotelController.js";
 
+const router = express.Router()
+
+//create new Hotel
+router.post('/',createHotel)
+
+//update Hotel
+router.put('/:id',updateHotel)
+
+//update Hotel
+router.delete('/:id',deleteHotel)
+
+//update Hotel
+router.get('/:id',getSingleHotel)
+
+//update Hotel
+router.get('/',getAllHotels)
+
+
+export default router
